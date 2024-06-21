523ec57a27291b2d41639c3a67ae0b5b56679a58
Danilo Costa <danilopscosta@gmail.com>
2024-06-21 15:49:29 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');//importando a biblioteca express
 const app = express();//crio um a const, fazendo uma instancia para usar como objeto para ter acesso aos seus metodos
 
+const mongoose = require('mongoose');//importando mogoose biblioteca para conexão ao bando de dados
+mongoose.connect('mongodb://127.0.0.1:27017/test');//Aqui eu criei a conexão
+
 const port = 3000//declaro a porta da minha maquina que  será usada
 
 app.get('/', (req, res) => {
